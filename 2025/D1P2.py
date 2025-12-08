bfd9eb56af9d21d5b64c6a30845aa39f79885d84
qnengn <244242274+qnengn87@users.noreply.github.com>
2025-12-08 14:28:53 +1100

    2025 - D1P2 working solution

--- 2025/D1P2.py
+++ 2025/D1P2.py
@@ -1,25 +1,9 @@
 import math
 
-contents = open("input-2.txt").read().splitlines()
+contents = open("input.txt").read().splitlines()
 
 dial_point = 50
 count_pass_zero = 0
-
-# starting dial start point = 50
-# left = -1 
-# right = +1
-# each dial has a distance value
-
-# dial change = (current dial value + (left or right * distance value))
-# if the mod of the number is 0, then add to count_at_zero
-# get current position by modding with 100 
-
-# day 2 
-# if 0 - counts as zero
-# if 0 is between the prev number and the current number after the dial change, counts as having gone past 0
-# or what is the smallest number change for it to count as zero, if it's greater or equal to that than counts as 0 
-## or what is the smallest change of that and get the floored of (smallest/distance) which will tell you how many times it would hit zero 
-# 
 
 def get_distance(line):
     distance = int(line[1:])
@@ -36,38 +20,28 @@ def change_dial(line, point):
 
 for line in contents:
     new_position, distance = change_dial(line, dial_point)
-    
-    min_distance_left = dial_point
-    min_distance_right = 100-dial_point
 
-    # if travelling to the left, distance needs to be larger than the minimum to reach 0
-    if distance < 0 and abs(distance) >= min_distance_left:
-        if dial_point == 0 and (abs(distance) < 100):
-            pass
+    # travelling to the right
+    if distance > 0:
+        count_pass_zero += new_position//100
+
+    # travelling to the left
+    if distance < 0:
+        if dial_point == 0:
+            min_distance_to_hit_zero = 100 #distance needs to be 100 if it starts at 0 to be considered a click, otherwise minimum distance is only 0
         else:
-            # something wrong here?
-            count_pass_zero += (abs(distance//100)) # distance because looking at rotations, otherwise you lose from the minus
+            min_distance_to_hit_zero = dial_point
+        # if dial has been turned enough to reach zero 
+        if abs(distance) >= min_distance_to_hit_zero:
+            count_pass_zero += 1
+        # now take only the distance from the minimum point to see how much more it has travelled, i.e. any more rotations
+            new_distance = abs(distance) - min_distance_to_hit_zero
+            count_pass_zero += new_distance//100
+            
     
-    if distance > 0 and abs(distance) >= min_distance_right:
-        if dial_point == 0 and (abs(distance) < 100):
-            pass
-        else:
-            if new_position%100 == 0  and dial_point != 0:
-                count_pass_zero += 1
-            else:
-                count_pass_zero += (abs((new_position//100))) # new position because rotations
-    
-    # if new_position == 0 and dial_point != 0:
-    #     count_pass_zero += 1
-    
-    
-    #count_pass_zero += (abs(new_position//100))
-    
-    print(line, dial_point, new_position, distance, min_distance_left, min_distance_right, (new_position%100), count_pass_zero)
+    print(line, dial_point, distance, (new_position%100), count_pass_zero)
     
     dial_point = new_position%100
-    
-    # print(dial_point)
 
 print(count_pass_zero)
 
